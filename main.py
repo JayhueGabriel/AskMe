5e49ca39dcf41a65b020ba1fb64b0137b0dc3a6a
JayhueGabriel <gabriel.238@buckeyemail.osu.edu>
2026-04-12 14:14:38 -0400

    Removed setup functions + setup textbox

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import mss
 
 ctk.set_appearance_mode("dark")
 root = ctk.CTk()
-responseTab = ctk.CTkToplevel()
+responseTab = ctk.CTkToplevel(root)
 responseTab.withdraw()
 
 searchWidth = 600
@@ -16,28 +16,36 @@ screen_h = root.winfo_screenheight()
 x = int(screen_w / 2)
 y = int(screen_h / 2)
 
-def setUpWindow():
-    root.geometry(f"{searchWidth}x{searchHeight}+{x}+{y}")
-    # Apply rounded corners via Windows API
-    HWND = windll.user32.GetForegroundWindow()
-    windll.dwmapi.DwmSetWindowAttribute(HWND, 33, byref(c_int(2)), sizeof(c_int))
-    root.overrideredirect(True)
-    root.wm_attributes('-topmost', True)
-
 def lookUp():
+    # Grab screenshot
     root.withdraw()
+    responseTab.withdraw()
     with mss.mss() as sct:
         sct.shot()
-    print(searchBar.get())
-    searchBar.delete(0, "end")
-    setUpResponseWindow()
-    root.deiconify()  
 
-def setUpResponseWindow():
-    responseTab.geometry(f"{responseWidth}x{responseHeight}+{x}+{y + 200}")
-    responseTab.overrideredirect(True)
+    # Set textbox
+    textbox.configure(state="normal")
+    textbox.delete("1.0", "end")
+    textbox.insert("end", searchBar.get())
+    textbox.see("1.0")
+    textbox.configure(state="disabled")
+
+    # Clear search and appear
+    searchBar.delete(0, "end")
+    root.deiconify()  
     responseTab.deiconify()
 
+
+#Create window size + poistion
+root.geometry(f"{searchWidth}x{searchHeight}+{x}+{y}")
+responseTab.geometry(f"{responseWidth}x{responseHeight}+{x}+{y + 200}")
+# Remove title & force to front
+root.overrideredirect(True)
+responseTab.overrideredirect(True)
+root.wm_attributes('-topmost', True)
+
+textbox = ctk.CTkTextbox(responseTab, width=responseWidth, height=responseHeight, font=('Arial', 14))
+textbox.pack()
 
 root.bind("<Escape>", lambda e: root.destroy())
 root.bind("<Return>", lambda e: lookUp() )
@@ -45,8 +53,4 @@ root.bind("<Return>", lambda e: lookUp() )
 searchBar = ctk.CTkEntry(root, width=searchWidth, font=("Arial", 25), placeholder_text="Hmm...", border_width=0, fg_color= "#212121")
 searchBar.pack(pady=10)
 
-
-
-setUpWindow()
 root.mainloop()
-responseTab.mainloop()
